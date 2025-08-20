f8a05f5daedc259def4dfd3a4c0f0132268c0d87
Dair H. <168670895+DairHX@users.noreply.github.com>
2025-08-20 01:21:53 +0100

    Update Jwt_Public_key_Hmac_Bypass.py

--- JWT/Jwt_Public_key_Hmac_Bypass.py
+++ JWT/Jwt_Public_key_Hmac_Bypass.py
@@ -33,7 +33,7 @@ message = header_b64 + b'.' + payload_b64
 
 # Step 4: Use the public key as the HMAC secret
 public_key = b"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDHSoarRoLvgAk4O41RE0w6lj2e7TDTbFk62WvIdJFo/aSLX/x9oc3PDqJ0Qu1x06/8PubQbCSLfWUyM7Dk0+irzb/VpWAurSh+hUvqQCkHmH9mrWpMqs5/L+rluglPEPhFwdL5yWk5kS7rZMZz7YaoYXwI7Ug4Es4iYbf6+UV0sudGwc3HrQ5uGUfOpmixUO0ZgTUWnrfMUpy2dFbZp7puQS6T8b5EJPpLY+iojMb/rbPB34NrvJKU1F84tfvY8xtg3HndTNPyNWp7EOsujKZIxKF5/RdW+Qf9jjBMvsbjfCo0LiNVjpotiLPVuslsEWun+LogxR+fxLiUehSBb8ip"
-#
+
   
 
 # Step 5: Create HMAC SHA256 signature (The Vulnerability) !!!
